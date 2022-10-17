5a16e3823bc31931e7450ea207ed9f86dfcc106b
Ivan Basov <vanya.b94@mail.ru>
2022-10-17 13:22:07 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+import './index.scss';
+
+import Modal from './components';
+
+const App: React.FC = () => {
+
+   const [open, setOpen] = useState<boolean>(false);
+
+   return (
+      <div className="App">
+         <button
+            onClick={() => setOpen(true)}
+            className="open-modal-btn"
+         >
+            ✨ Открыть окно
+         </button>
+         <Modal
+            open={open}
+            setOpen={setOpen}
+         />
+      </div>
+   );
+};
+
+export default App;
